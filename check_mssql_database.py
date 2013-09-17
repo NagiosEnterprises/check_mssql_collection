41cc4604cd04ed7eba7dd50144b8ae19b9bf45e5
James Orr <orrjamesw@gmail.com>
2013-09-17 11:25:29 -0400

    Fixes typo in datasize query.  Previous datasize query used log growth query instead.

--- check_mssql_database.py
+++ check_mssql_database.py
@@ -103,7 +103,7 @@ MODES     = {
     'datasize'          : { 'help'      : 'Database Size',
                             'stdout'    : 'Database size is %sKB',
                             'label'     : 'KB',
-                            'query'     : BASE_QUERY % 'Log Growths',
+                            'query'     : BASE_QUERY % 'Data File(s) Size (KB)',
                             'type'      : 'standard'
                             },
     
